6190e2017eda7e59a84987427274bc495b5f7e26
nakamarusun <nakamarujc@gmail.com>
2019-10-19 00:42:48 +0700

    Added LoadingScreen, simple tab bar, and bare-bones home

--- UniClass/ViewController.swift
+++ UniClass/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("View loaded")
+        print("[DEBUG MESSAGE] View loaded.")
         // Do any additional setup after loading the view.
     }
 
