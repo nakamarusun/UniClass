ee962e112f549a86439126fcef552bbcab99c21f
nakamarusun <nakamarujc@gmail.com>
2019-10-18 22:03:43 +0700

    Initial Commit

---
+++ UniClass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniClass
+//
+//  Created by Jason Christian Hailianto on 18/10/19.
+//  Copyright © 2019 Jobba Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
