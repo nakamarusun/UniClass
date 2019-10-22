5ccfbee7f91fe2bd3bcf358d04d75004264bba05
nakamarusun <nakamarujc@gmail.com>
2019-10-22 23:24:49 +0700

    Attempt to make collection view

---
+++ UniClass/Home/whatshotCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  whatshotCollectionViewCell.swift
+//  UniClass
+//
+//  Created by Jason Christian Hailianto on 22/10/19.
+//  Copyright © 2019 Jobba Inc. All rights reserved.
+//
+
+import UIKit
+
+class whatshotCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var cellImage: UIImageView!
+}
