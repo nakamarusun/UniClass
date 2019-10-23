14df49cbdfc204d042e2b3c73be7522e726461ab
john <johndoe@gmail.com>
2019-10-23 15:25:49 +0700

    Note 2.0 subtopic

--- UniClass/Notebook/More/NotebookSpecificTopics.swift
+++ UniClass/Notebook/More/NotebookSpecificTopics.swift
@@ -42,7 +42,40 @@ class NotebookSpecificTopics: NotebookViewTable {
         
         switch selectedCell.cellName {
             case "Matematika":
-                cellArray.append(StandardCell(cellName: "Integral", cellSubtitle: "Non", cellImage: UIImage(named: "Untitled-1")!))
+                cellArray.append(StandardCell(cellName: "Aljabar", cellSubtitle: "Non", cellImage: UIImage(named: "math3")!))
+                cellArray.append(StandardCell(cellName: "Kalkulus", cellSubtitle: "Non", cellImage: UIImage(named: "math2")!))
+                cellArray.append(StandardCell(cellName: "Integral", cellSubtitle: "Non", cellImage: UIImage(named: "math4")!))
+            cellArray.append(StandardCell(cellName: "Trigonometri", cellSubtitle: "Non", cellImage: UIImage(named: "math5")!))
+            cellArray.append(StandardCell(cellName: "Statistika", cellSubtitle: "Non", cellImage: UIImage(named: "math6")!))
+            
+        case "Fisika":
+            cellArray.append(StandardCell(cellName: "Kesetimbangan dan Dinamika Rotasi", cellSubtitle: "Non", cellImage: UIImage(named: "fisika2")!))
+            cellArray.append(StandardCell(cellName: "Elasititas dan Hukum Hooke", cellSubtitle: "Non", cellImage: UIImage(named: "fisika3")!))
+            cellArray.append(StandardCell(cellName: "Fluida Statis", cellSubtitle: "Non", cellImage: UIImage(named: "fisika1")!))
+            
+        case "Kimia":
+            cellArray.append(StandardCell(cellName: "Materi", cellSubtitle: "Non", cellImage: UIImage(named: "kimia2")!))
+            cellArray.append(StandardCell(cellName: "Materi", cellSubtitle: "Non", cellImage: UIImage(named: "kimia4")!))
+            cellArray.append(StandardCell(cellName: "Sistem Periodik", cellSubtitle: "Non", cellImage: UIImage(named: "kimia1")!))
+            
+            
+        case "Biologi":
+            cellArray.append(StandardCell(cellName: "Virus", cellSubtitle: "Non", cellImage: UIImage(named: "biologi2")!))
+            cellArray.append(StandardCell(cellName: "Fungi", cellSubtitle: "Non", cellImage: UIImage(named: "biologi3")!))
+            cellArray.append(StandardCell(cellName: "Plantae", cellSubtitle: "Non", cellImage: UIImage(named: "biologi1")!))
+        case "Bahasa Inggris":
+            cellArray.append(StandardCell(cellName: "English Expression", cellSubtitle: "Non", cellImage: UIImage(named: "bahasaInggris2")!))
+            cellArray.append(StandardCell(cellName: "Texts", cellSubtitle: "Non", cellImage: UIImage(named: "bahasaInggris3")!))
+            cellArray.append(StandardCell(cellName: "Grammar", cellSubtitle: "Non", cellImage: UIImage(named: "bahasaInggris1")!))
+      
+        case "Bahasa Indonesia":
+            cellArray.append(StandardCell(cellName: "Ide Pokok", cellSubtitle: "Non", cellImage: UIImage(named: "bahasaIndonesia2")!))
+            cellArray.append(StandardCell(cellName: "Memahami dan Menanggapi Bacaan", cellSubtitle: "Non", cellImage: UIImage(named: "bahasaIndonesia3")!))
+            cellArray.append(StandardCell(cellName: "Fakta dan Tanggapan dalam Teks", cellSubtitle: "Non", cellImage: UIImage(named: "bahasaIndonesia1")!))
+
+            
+            
+
             
             default:
                 cellArray.append(StandardCell())
